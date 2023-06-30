faf5bf0be909a4abd5543ed985a69a98de27d928
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2023-06-30 16:19:59 +0200

    fixed formatting

--- src/HASS.Agent.Staging/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/LastActiveSensor.cs
+++ src/HASS.Agent.Staging/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/LastActiveSensor.cs
@@ -9,107 +9,107 @@ using HASS.Agent.Shared.Models.HomeAssistant;
 
 namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue
 {
-	/// <summary>
-	/// Sensor containing the last moment the user provided any input
-	/// </summary>
-	public class LastActiveSensor : AbstractSingleValueSensor
-	{
-		private const string DefaultName = "lastactive";
+    /// <summary>
+    /// Sensor containing the last moment the user provided any input
+    /// </summary>
+    public class LastActiveSensor : AbstractSingleValueSensor
+    {
+        private const string DefaultName = "lastactive";
 
-		private DateTime _lastActive = DateTime.MinValue;
+        private DateTime _lastActive = DateTime.MinValue;
 
-		public const int DefaultTimeWindow = 15;
+        public const int DefaultTimeWindow = 15;
 
-		public bool ApplyRounding { get; private set; }
-		public int Round { get; private set; }
+        public bool ApplyRounding { get; private set; }
+        public int Round { get; private set; }
 
-		public LastActiveSensor(bool updateOnResume, int? updateOnResumeTimeWindow, int? updateInterval = 10, string name = DefaultName, string friendlyName = DefaultName, string id = default) : base(name ?? DefaultName, friendlyName ?? null, updateInterval ?? 10, id)
-		{
-			ApplyRounding = updateOnResume;
-			Round = updateOnResumeTimeWindow ?? 30;
-		}
+        public LastActiveSensor(bool updateOnResume, int? updateOnResumeTimeWindow, int? updateInterval = 10, string name = DefaultName, string friendlyName = DefaultName, string id = default) : base(name ?? DefaultName, friendlyName ?? null, updateInterval ?? 10, id)
+        {
+            ApplyRounding = updateOnResume;
+            Round = updateOnResumeTimeWindow ?? 30;
+        }
 
-		public override DiscoveryConfigModel GetAutoDiscoveryConfig()
-		{
-			if (Variables.MqttManager == null)
-				return null;
+        public override DiscoveryConfigModel GetAutoDiscoveryConfig()
+        {
+            if (Variables.MqttManager == null)
+                return null;
 
-			var deviceConfig = Variables.MqttManager.GetDeviceConfigModel();
-			if (deviceConfig == null)
-				return null;
+            var deviceConfig = Variables.MqttManager.GetDeviceConfigModel();
+            if (deviceConfig == null)
+                return null;
 
-			return AutoDiscoveryConfigModel ?? SetAutoDiscoveryConfigModel(new SensorDiscoveryConfigModel()
-			{
-				Name = Name,
-				FriendlyName = FriendlyName,
-				Unique_id = Id,
-				Device = deviceConfig,
-				State_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/state",
-				Icon = "mdi:clock-time-three-outline",
-				Availability_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/availability",
-				Device_class = "timestamp"
-			});
-		}
+            return AutoDiscoveryConfigModel ?? SetAutoDiscoveryConfigModel(new SensorDiscoveryConfigModel()
+            {
+                Name = Name,
+                FriendlyName = FriendlyName,
+                Unique_id = Id,
+                Device = deviceConfig,
+                State_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/state",
+                Icon = "mdi:clock-time-three-outline",
+                Availability_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/availability",
+                Device_class = "timestamp"
+            });
+        }
 
-		public override string GetState()
-		{
-			var lastInput = GetLastInputTime();
+        public override string GetState()
+        {
+            var lastInput = GetLastInputTime();
 
-			if (ApplyRounding)
-			{
-				if (SharedSystemStateManager.LastEventOccurrence.TryGetValue(Enums.SystemStateEvent.Resume, out var lastWakeEventDate) // was there a wake event
-					&& DateTime.Compare(lastInput, lastWakeEventDate) < 0 // was the last input before the last wake event
-					&& (DateTime.Now - lastWakeEventDate).TotalSeconds < Round) // are we within the time window
-				{
+            if (ApplyRounding)
+            {
+                if (SharedSystemStateManager.LastEventOccurrence.TryGetValue(Enums.SystemStateEvent.Resume, out var lastWakeEventDate) // was there a wake event
+                    && DateTime.Compare(lastInput, lastWakeEventDate) < 0 // was the last input before the last wake event
+                    && (DateTime.Now - lastWakeEventDate).TotalSeconds < Round) // are we within the time window
+                {
 
-					var currentPosition = Cursor.Position;
-					Cursor.Position = new Point(Cursor.Position.X - 10, Cursor.Position.Y - 10);
-					Cursor.Position = currentPosition;
+                    var currentPosition = Cursor.Position;
+                    Cursor.Position = new Point(Cursor.Position.X - 10, Cursor.Position.Y - 10);
+                    Cursor.Position = currentPosition;
 
-					lastInput = GetLastInputTime();
-				}
-			}
+                    lastInput = GetLastInputTime();
+                }
+            }
 
-			// changed to min. 1 sec difference
-			// source: https://github.com/sleevezipper/hass-workstation-service/pull/156
-			if ((_lastActive - lastInput).Duration().TotalSeconds > 1)
-				_lastActive = lastInput;
+            // changed to min. 1 sec difference
+            // source: https://github.com/sleevezipper/hass-workstation-service/pull/156
+            if ((_lastActive - lastInput).Duration().TotalSeconds > 1)
+                _lastActive = lastInput;
 
-			return _lastActive.ToTimeZoneString();
-		}
+            return _lastActive.ToTimeZoneString();
+        }
 
-		public override string GetAttributes() => string.Empty;
+        public override string GetAttributes() => string.Empty;
 
-		private static DateTime GetLastInputTime()
-		{
-			var lastInputInfo = new LASTINPUTINFO();
-			lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
-			lastInputInfo.dwTime = 0;
+        private static DateTime GetLastInputTime()
+        {
+            var lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
 
-			var envTicks = Environment.TickCount;
+            var envTicks = Environment.TickCount;
 
-			if (!GetLastInputInfo(ref lastInputInfo))
-				return DateTime.Now;
+            if (!GetLastInputInfo(ref lastInputInfo))
+                return DateTime.Now;
 
-			var lastInputTick = Convert.ToDouble(lastInputInfo.dwTime);
+            var lastInputTick = Convert.ToDouble(lastInputInfo.dwTime);
 
-			var idleTime = envTicks - lastInputTick;
-			return idleTime > 0 ? DateTime.Now - TimeSpan.FromMilliseconds(idleTime) : DateTime.Now;
-		}
+            var idleTime = envTicks - lastInputTick;
+            return idleTime > 0 ? DateTime.Now - TimeSpan.FromMilliseconds(idleTime) : DateTime.Now;
+        }
 
-		[DllImport("User32.dll")]
-		private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
 
-		[StructLayout(LayoutKind.Sequential)]
-		// ReSharper disable once InconsistentNaming
-		private struct LASTINPUTINFO
-		{
-			private static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
+        [StructLayout(LayoutKind.Sequential)]
+        // ReSharper disable once InconsistentNaming
+        private struct LASTINPUTINFO
+        {
+            private static readonly int SizeOf = Marshal.SizeOf(typeof(LASTINPUTINFO));
 
-			[MarshalAs(UnmanagedType.U4)]
-			public int cbSize;
-			[MarshalAs(UnmanagedType.U4)]
-			public uint dwTime;
-		}
-	}
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public uint dwTime;
+        }
+    }
 }
