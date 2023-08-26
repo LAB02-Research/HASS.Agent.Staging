93071c4eea6403e0dbcb375d43f82a5fb0344d11
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2023-08-26 22:18:44 +0200

    cleanup

--- src/HASS.Agent.Staging/HASS.Agent.Satellite.Service/Commands/CommandsManager.cs
+++ src/HASS.Agent.Staging/HASS.Agent.Satellite.Service/Commands/CommandsManager.cs
@@ -59,7 +59,7 @@ namespace HASS.Agent.Satellite.Service.Commands
                 foreach (var command in Variables.Commands)
                 {
                     await command.UnPublishAutoDiscoveryConfigAsync();
-                    await Variables.MqttManager.UnubscribeAsync(command);
+                    await Variables.MqttManager.UnsubscribeAsync(command);
                     command.ClearAutoDiscoveryConfig();
                     count++;
                 }
@@ -188,7 +188,7 @@ namespace HASS.Agent.Satellite.Service.Commands
 
                         // remove and unregister
                         await abstractCommand.UnPublishAutoDiscoveryConfigAsync();
-                        await Variables.MqttManager.UnubscribeAsync(abstractCommand);
+                        await Variables.MqttManager.UnsubscribeAsync(abstractCommand);
                         Variables.Commands.RemoveAt(Variables.Commands.FindIndex(x => x.Id == abstractCommand.Id));
 
                         Log.Information("[COMMANDS] Removed command: {command}", abstractCommand.Name);
@@ -220,7 +220,7 @@ namespace HASS.Agent.Satellite.Service.Commands
                         Log.Information("[COMMANDS] Command changed, re-registering as new entity: {old} to {new}", Variables.Commands[currentCommandIndex].Name, abstractCommand.Name);
 
                         await Variables.Commands[currentCommandIndex].UnPublishAutoDiscoveryConfigAsync();
-                        await Variables.MqttManager.UnubscribeAsync(Variables.Commands[currentCommandIndex]);
+                        await Variables.MqttManager.UnsubscribeAsync(Variables.Commands[currentCommandIndex]);
                         await Variables.MqttManager.SubscribeAsync(abstractCommand);
                     }
 
